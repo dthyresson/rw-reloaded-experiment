b4726b932b106a1aa6a467607d198eb4f5f5da21
David Thyresson <dthyresson@gmail.com>
2025-02-19 10:09:05 -0500

    try tailwindcss

--- experiments/pitch/src/app/Document.tsx
+++ experiments/pitch/src/app/Document.tsx
@@ -1,9 +1,14 @@
-export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+import stylesUrl from "./style.css?url";
+
+export const Document: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>@redwoodjs/starter-minimal</title>
+      <title>Pitch: The AI-Powered Sales Tool</title>
+      <link rel="stylesheet" href={stylesUrl} />
       <script type="module" src="/src/client.tsx"></script>
     </head>
     <body>
--- experiments/pitch/src/app/pages/Home.tsx
+++ experiments/pitch/src/app/pages/Home.tsx
@@ -3,11 +3,11 @@ export async function Home() {
   const users = await db.user.findMany();
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>Hello World</h1>
       <pre>{JSON.stringify(users, null, 2)}</pre>
       {users.map((user) => (
-        <ul key={user.id}>
+        <ul key={user.id} className="text-3xl font-bold underline">
           <li>{user.email}</li>
         </ul>
       ))}
