49908fa9c40274269a83f3c9c3d0b8d73bc7aa41
David Thyresson <dthyresson@gmail.com>
2025-02-20 08:57:48 -0500

    WIP

--- experiments/pitch/src/app/pages/Home.tsx
+++ experiments/pitch/src/app/pages/Home.tsx
@@ -1,16 +1,35 @@
 import { db } from "@/db";
+// import type { User } from "@prisma/client";\
+// import type { User } from "@prisma/client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import { Button } from "@/app/components/ui/button";
 export async function Home() {
   const users = await db.user.findMany();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Hello World</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-      {users.map((user) => (
-        <ul key={user.id} className="text-3xl font-bold underline">
-          <li>{user.email}</li>
-        </ul>
-      ))}
+    <div className="container mx-auto p-12">
+      <Card>
+        <CardHeader>
+          <CardTitle>Hello World</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className="my-4">
+            {users.map((user: any) => (
+              <li key={user.id}>{user.email}</li>
+            ))}
+          </ul>
+          <CardFooter>
+            <Button>Click me</Button>
+          </CardFooter>
+        </CardContent>
+      </Card>
     </div>
   );
 }
